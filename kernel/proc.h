8498770f54b69a5ca324970f4aab782764a3e3fc
GrantHee <1796954825@qq.com>
2023-07-14 12:37:12 +0800

    traps

--- kernel/proc.h
+++ kernel/proc.h
@@ -105,4 +105,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint64 interval;             // the time of alarm
+  void(*handler)();            // alarm handle function
+  uint64 spend;                // record the time of how many ticks has passed
+  struct trapframe *trapframeSave;
+  int waitReturn;              // if wait return 1;not return 0
 };
